f965803d1e3e9ea86e12f6c2f0de7aab26d47582
whoopscs <133087009+oopsunix@users.noreply.github.com>
2025-03-09 00:58:33 +0800

    Add umami backtotop

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,28 +1,43 @@
 // .vitepress/theme/index.ts
 import DefaultTheme from "vitepress/theme";
-
+import { h } from 'vue'
+import CommentWaline from './components/CommentWaline.vue'
+import backtotop from "./components/backtotop.vue"
 import "./style/index.css"; //引入自定义的样式
 
-// 图片缩放插件
-import mediumZoom from 'medium-zoom';
-import { onMounted, watch, nextTick } from 'vue';
-import { useRoute } from 'vitepress';
+import { umamiAnalytics } from '@theojs/lumen'
 
+// 图片缩放插件
+import mediumZoom from "medium-zoom";
+import { onMounted, watch, nextTick } from "vue";
+import { useRoute } from "vitepress";
 
 export default {
-    extends: DefaultTheme,
-    setup() {
-        const route = useRoute();
-        const initZoom = () => {
-          // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
-          mediumZoom(".main img", { background: "var(--vp-c-bg)" }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
-        };
-        onMounted(() => {
-          initZoom();
-        });
-        watch(
-          () => route.path,
-          () => nextTick(() => initZoom())
-        );
-      },
-  };
+  extends: DefaultTheme,
+  enhanceApp: ({ app }) => {
+    umamiAnalytics({
+      id: "a50390ff-13be-4347-b9b5-b2c08d6892b8",
+      src: "https://umami.akams.cn/script.js",
+    });
+  },
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
+      mediumZoom(".main img", { background: "var(--vp-c-bg)" }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'doc-after': () => h(CommentWaline), // 评论插件
+      'doc-footer-before': () => h(backtotop), // 回到顶部插件
+    })
+  }
+};
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
 - [**VitePress Sidebar**](https://vitepress-sidebar.cdget.com/zhHans/)：VitePress自动侧边栏插件
 - [**VitePress Auto SideBar Plugin**](https://vitepress-auto-sidebar-plugin.netlify.app/)：VitePress 自动侧边栏插件
+- [**Lumen**](https://github.com/Theo-Messi/lumen): 集成 Vue 功能组件和主题美化的 VitePress 插件
